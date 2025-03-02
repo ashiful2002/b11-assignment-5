863bac03e108c401dae284ed46f8b97767931768
ashiful-islam <ashifulislam2002@gmail.com>
2025-03-02 19:35:32 +0600

    second commit

--- scripts/taskManager.js
+++ scripts/taskManager.js
@@ -27,3 +27,6 @@ document.getElementById("clear-history").addEventListener("click", function () {
   alert("are you sure to delete?");
   document.getElementById("activity-container").innerText = "";
 });
+
+
+
